225c627486e31a9c268909bef2667b269bb7866b
grovas <arczir@gmail.com>
2017-06-13 20:27:29 +0200

    initial commit

---
+++ src/main/java/com/sdajava/Horda/HordaApplication.java
@@ -0,0 +1,12 @@
+package com.sdajava.Horda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HordaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HordaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sdajava/Horda/HordaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sdajava.Horda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HordaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
