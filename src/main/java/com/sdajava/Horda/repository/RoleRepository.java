ba8cf144dcc1d4b12537f4eaec21643c905c47ab
grovas <arczir@gmail.com>
2017-06-20 20:17:46 +0200

    Working version 4U

--- src/main/java/com/sdajava/Horda/repository/RoleRepository.java
+++ src/main/java/com/sdajava/Horda/repository/RoleRepository.java
@@ -1,13 +1,13 @@
-package com.sdajava.Horda.repository;
-
-import com.sdajava.Horda.model.Role;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
-    @Query("SELECT r FROM Role r INNER JOIN r.users u WHERE u.username = ?1")
-    Role findRoleByUsername(String username);
-
-}
+//package com.sdajava.Horda.repository;
+//
+//import com.sdajava.Horda.model.Role;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface RoleRepository extends CrudRepository<Role, Long> {
+//    @Query("SELECT r FROM Role r INNER JOIN r.users u WHERE u.username = ?1")
+//    Role findRoleByUsername(String username);
+//
+//}
