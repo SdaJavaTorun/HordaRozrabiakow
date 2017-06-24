caddc6e1f97732c7085b6df3c6f5d3371d995a59
grovas <arczir@gmail.com>
2017-06-24 22:37:52 +0200

    added bootstrap to main.jsp

--- src/main/java/com/sdajava/Horda/config/MvcConfig.java
+++ src/main/java/com/sdajava/Horda/config/MvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/user/registration").setViewName("register");
